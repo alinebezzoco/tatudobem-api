8f830e64c79abe407abce01a65d4d818b29d0757
Aline Bezzoco <contato.alinebezzoco@gmail.com>
2019-08-25 15:50:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const listRoutes = require("./api/routes/ListRoutes");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // database connection
 require("./config/db");
