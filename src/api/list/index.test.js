9c3a84536f23d57892d926e1f5c7e12203b5c0f9
Aline Bezzoco <contato.alinebezzoco@gmail.com>
2019-08-25 23:44:24 -0300

    update test

--- src/api/list/index.test.js
+++ src/api/list/index.test.js
@@ -11,7 +11,7 @@ beforeEach(inject(async () => {
   list = await List.create({})
 }));
 
-test('POST /lists 201', async () => {
+test(inject('POST /lists 201', async () => {
   const { status, body } = await request(app())
     .post(`${apiRoot}`)
     .send({ name: 'test', description: 'test', address: 'test', phone: 'test', email: 'test', city: 'test', uf: 'test' })
@@ -24,31 +24,34 @@ test('POST /lists 201', async () => {
   expect(body.email).toEqual('test')
   expect(body.city).toEqual('test')
   expect(body.uf).toEqual('test')
-})
+}));
 
-test('GET /lists 200', async () => {
+test(inject('GET /lists 200', async () => { 
   const { status, body } = await request(app())
     .get(`${apiRoot}`)
   expect(status).toBe(200)
   expect(Array.isArray(body.results)).toBe(true)
   expect(Number.isNaN(body.count)).toBe(false)
-})
+}));
 
-test('GET /lists/:id 200', async () => {
+
+test(inject('GET /lists/:id 200', async () => { 
   const { status, body } = await request(app())
     .get(`${apiRoot}/${list.id}`)
   expect(status).toBe(200)
   expect(typeof body).toEqual('object')
   expect(body.id).toEqual(list.id)
-})
+}));
 
-test('GET /lists/:id 404', async () => {
-  const { status } = await request(app())
+
+test(inject('GET /lists/:id 404', async () => { 
+    const { status } = await request(app())
     .get(apiRoot + '/123456789098765432123456')
   expect(status).toBe(404)
-})
+}));
 
-test('PUT /lists/:id 200', async () => {
+
+test(inject('PUT /lists/:id 200', async () => { 
   const { status, body } = await request(app())
     .put(`${apiRoot}/${list.id}`)
     .send({ name: 'test', description: 'test', address: 'test', phone: 'test', email: 'test', city: 'test', uf: 'test' })
@@ -62,23 +65,25 @@ test('PUT /lists/:id 200', async () => {
   expect(body.email).toEqual('test')
   expect(body.city).toEqual('test')
   expect(body.uf).toEqual('test')
-})
+}));
 
-test('PUT /lists/:id 404', async () => {
+
+test(inject('PUT /lists/:id 404', async () => { 
   const { status } = await request(app())
     .put(apiRoot + '/123456789098765432123456')
     .send({ name: 'test', description: 'test', address: 'test', phone: 'test', email: 'test', city: 'test', uf: 'test' })
   expect(status).toBe(404)
-})
+}));
 
-test('DELETE /lists/:id 204', async () => {
+
+test(inject('DELETE /lists/:id 204', async () => { 
   const { status } = await request(app())
     .delete(`${apiRoot}/${list.id}`)
   expect(status).toBe(204)
-})
+}));
 
-test('DELETE /lists/:id 404', async () => {
+test(inject('DELETE /lists/:id 204', async () => { 
   const { status } = await request(app())
     .delete(apiRoot + '/123456789098765432123456')
   expect(status).toBe(404)
-})
+}));
